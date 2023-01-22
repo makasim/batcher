1442caf04c69a01c40b3b49c957a6b28064ad9fe
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2023-01-22 12:04:42 +0200

    t -> item

--- batcher.go
+++ batcher.go
@@ -5,8 +5,8 @@ import (
 	"time"
 )
 
-type Batcher[T any] struct {
-	collectorCh chan T
+type Batcher[Item any] struct {
+	collectorCh chan Item
 	promoteT    *time.Timer
 
 	counter int64
@@ -14,34 +14,34 @@ type Batcher[T any] struct {
 	active  int64
 }
 
-func New[T any](batch int64) *Batcher[T] {
+func New[Item any](batch int64) *Batcher[Item] {
 	if batch <= 0 {
 		panic("batch must be greater than zero")
 	}
 
-	return &Batcher[T]{
-		collectorCh: make(chan T, batch*10),
+	return &Batcher[Item]{
+		collectorCh: make(chan Item, batch*10),
 		promoteT:    time.NewTimer(time.Millisecond * 100),
 		batch:       batch,
 	}
 }
 
-func (b *Batcher[T]) Batch(t T) []T {
+func (b *Batcher[Item]) Batch(item Item) []Item {
 	idx := atomic.AddInt64(&b.counter, 1)
 
 	if idx%(b.batch) == 0 {
-		return b.collect(t)
+		return b.collect(item)
 	}
 
 	select {
-	case b.collectorCh <- t:
+	case b.collectorCh <- item:
 		return nil
 	case <-b.promoteT.C:
-		return b.collect(t)
+		return b.collect(item)
 	}
 }
 
-func (b *Batcher[T]) collect(t T) []T {
+func (b *Batcher[Item]) collect(item Item) []Item {
 	if cnt := atomic.AddInt64(&b.active, 1); cnt == 1 {
 		stopAndDrainTimer(b.promoteT)
 	}
@@ -53,16 +53,16 @@ func (b *Batcher[T]) collect(t T) []T {
 		}
 	}()
 
-	res := make([]T, 0, b.batch)
-	res = append(res, t)
+	res := make([]Item, 0, b.batch)
+	res = append(res, item)
 	for {
 		if int64(len(res)) >= b.batch {
 			return res
 		}
 
 		select {
-		case t := <-b.collectorCh:
-			res = append(res, t)
+		case item := <-b.collectorCh:
+			res = append(res, item)
 		default:
 			return res
 		}
