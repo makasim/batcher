c92b9e00835cd2bab8f2257b48a0ff0bd6956127
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2023-02-28 21:31:04 +0200

    add tests

--- batcher_test.go
+++ batcher_test.go
@@ -10,28 +10,72 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestSizeOneRateOne(t *testing.T) {
-	b := batcher.New[int64](1, time.Millisecond*2)
+func TestSizeOne(main *testing.T) {
+	main.Run("RateOne", func(t *testing.T) {
+		b := batcher.New[int64](1, time.Second)
 
-	result := genLoad(b, 1, 100, time.Millisecond)
+		result := genLoad(b, 1, 100, time.Millisecond)
 
-	require.Equal(t, int64(200), result)
+		require.Equal(t, int64(200), result)
+	})
+
+	main.Run("RateTwo", func(t *testing.T) {
+		b := batcher.New[int64](1, time.Second)
+
+		result := genLoad(b, 2, 100, time.Millisecond)
+
+		require.Equal(t, int64(400), result)
+	})
+
+	main.Run("RateFive", func(t *testing.T) {
+		b := batcher.New[int64](1, time.Second)
+
+		result := genLoad(b, 5, 100, time.Millisecond)
+
+		require.Equal(t, int64(1000), result)
+	})
+
+	main.Run("RateTen", func(t *testing.T) {
+		b := batcher.New[int64](1, time.Second)
+
+		result := genLoad(b, 10, 100, time.Millisecond)
+
+		require.Equal(t, int64(2000), result)
+	})
 }
 
-func TestSizeOneRateTwo(t *testing.T) {
-	b := batcher.New[int64](1, time.Millisecond*2)
+func TestSizeTwo(main *testing.T) {
+	main.Run("RateTwo", func(t *testing.T) {
+		b := batcher.New[int64](2, time.Second)
 
-	result := genLoad(b, 2, 100, time.Millisecond)
+		result := genLoad(b, 2, 100, time.Millisecond)
 
-	require.Equal(t, int64(400), result)
-}
+		require.Equal(t, int64(400), result)
+	})
 
-func TestSizeOneRateTen(t *testing.T) {
-	b := batcher.New[int64](1, time.Millisecond*2)
+	main.Run("RateFour", func(t *testing.T) {
+		b := batcher.New[int64](2, time.Millisecond*100)
 
-	result := genLoad(b, 10, 100, time.Millisecond)
+		result := genLoad(b, 4, 100, time.Millisecond)
 
-	require.Equal(t, int64(2000), result)
+		require.Equal(t, int64(800), result)
+	})
+
+	main.Run("RateSix", func(t *testing.T) {
+		b := batcher.New[int64](2, time.Second)
+
+		result := genLoad(b, 6, 100, time.Millisecond)
+
+		require.Equal(t, int64(1200), result)
+	})
+
+	main.Run("RateTen", func(t *testing.T) {
+		b := batcher.New[int64](2, time.Millisecond*100)
+
+		result := genLoad(b, 10, 100, time.Millisecond)
+
+		require.Equal(t, int64(2000), result)
+	})
 }
 
 func TestSizeTenRateOne(t *testing.T) {
@@ -53,7 +97,7 @@ func TestSizeTenRateTwo(t *testing.T) {
 func TestSizeTenRateTen(t *testing.T) {
 	b := batcher.New[int64](10, time.Millisecond*2)
 
-	result := genLoad(b, 10, 1000, time.Millisecond)
+	result := genLoad(b, 10, 1000, time.Microsecond*100)
 
 	require.Equal(t, int64(20000), result)
 }
@@ -77,13 +121,17 @@ func genLoad(b *batcher.Batcher[int64], concr, reqs int, wait time.Duration) int
 			defer wg.Done()
 
 			for i := 0; i < reqs; i++ {
-				batch := b.Batch(2)
-				var lCnt int64
-				for _, item := range batch {
-					lCnt += item
-				}
+				wg.Add(1)
+				go func() {
+					defer wg.Done()
+					batch := b.Batch(2)
+					var lCnt int64
+					for _, item := range batch {
+						lCnt += item
+					}
 
-				atomic.AddInt64(&results, lCnt)
+					atomic.AddInt64(&results, lCnt)
+				}()
 				time.Sleep(wait)
 			}
 		}()
